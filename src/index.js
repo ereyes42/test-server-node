12c6167f74e5e0e6bb237d4d6b4d9d7b1ef940dd
Ember Reyes <ereyes42@stu.southtexascollege.edu>
2022-11-12 11:45:02 -0600

    test

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import './helpers/dotenv'
+
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import helmet from 'helmet'
+import bodyParser from 'body-parser'
+import basicAuth from 'express-basic-auth'
+
+import logger from './helpers/logger'
+import router from './routes'
+import { notFound, errorHandler } from './helpers/errors'
+
+const port = parseInt(process.env.PORT, 10) || 3000
+
+const app = express()
+
+app.use(
+  basicAuth({
+    users: { [process.env.ADMIN_USER]: process.env.ADMIN_PASSWORD },
+  }),
+)
+
+app.use(morgan(process.env.MORGAN_LOG))
+app.use(cors({ origin: process.env.ORIGIN }))
+app.use(helmet())
+app.use(bodyParser.json())
+
+app.use('/', router)
+
+app.use(notFound)
+app.use(errorHandler)
+
+app.listen(port, () =>
+  logger.info(`Application started at http://localhost:${process.env.PORT}`),
+)
